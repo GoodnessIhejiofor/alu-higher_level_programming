55e63e0de7b95f42443615c7ea8d14d241407ae3
Oma_goodness <99614883+GoodnessIhejiofor@users.noreply.github.com>
2022-11-22 12:30:13 +0200

    Update 4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-'''This script fetches https://intranet.hbtn.io/status.'''
+""" A Python script that:
+- takes in a URL,
+- sends a request to the URL and displays the body of the response.
+- (decoded in utf-8).
+"""
 import requests
 
+
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    res = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
