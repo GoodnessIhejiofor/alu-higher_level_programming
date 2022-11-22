8cc9e3ee6b0805554a8e682d05c6e1ff405d0eac
Oma_goodness <99614883+GoodnessIhejiofor@users.noreply.github.com>
2022-11-22 12:31:26 +0200

    Update 6-post_email.py

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-'''This script takes in a URL and an email address,
-sends a POST request to the passed URL with the email as a parameter,
-d, then isplays the body of the response
-'''
+"""A  script that:
+- takes in a URL and an email address
+- sends a POST request to the passed URL with the email as a parameter
+- displays the body of the response.
+"""
 import sys
 import requests
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
 
-    r = requests.post(url, data=value)
+if __name__ == "__main__":
+    res = sys.argv[1]
+    email = {"email": sys.argv[2]}
+
+    r = requests.post(res, data=email)
     print(r.text)
