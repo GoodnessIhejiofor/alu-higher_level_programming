2550fc130581b35298b73951579a64e5238cd8d9
Oma_goodness <99614883+GoodnessIhejiofor@users.noreply.github.com>
2022-12-13 12:03:04 +0200

    Update relationship_city.py

--- python-object_relational_mapping/relationship_city.py
+++ python-object_relational_mapping/relationship_city.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
+"""Script that lists all State objects from the database hbtn_0e_6_usa"""
 
-"""
-Defines a City model.
-Inherits from SQLAlchemy Base and links to the MySQL table cities.
-"""
-
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class City(Base):
-    """Represents a city for a MySQL database.
-    Attributes:
-        id (sqlalchemy.Column): The city's id.
-        name (sqlalchemy.Column): The city's name.
-        state_id (sqlalchemy.Column): The city's state id.
-    """
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
+    """class city"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
