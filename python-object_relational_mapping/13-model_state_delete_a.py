683378beb89c66c2ee1f639f11e68237e4013725
Oma_goodness <99614883+GoodnessIhejiofor@users.noreply.github.com>
2022-12-13 11:54:33 +0200

    Update 13-model_state_delete_a.py

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""
-changes the name of the State object where id=2 to New Mexico from a database
-"""
+"""Changes the name of a State object from the database"""
 
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
 from model_state import Base, State
+from sys import argv
 
 
 if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                    argv[2],
-                                                                    argv[3]))
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
+    # Create engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    # Create session
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
+    # Query
     states = session.query(State).filter(State.name.like('%a%'))
     for state in states:
         session.delete(state)
+    # Close session
     session.commit()
     session.close()
