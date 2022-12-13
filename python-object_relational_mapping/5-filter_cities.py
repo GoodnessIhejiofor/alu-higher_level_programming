b1551267453d5d9500362cbac765926e2430a6d2
Oma_goodness <99614883+GoodnessIhejiofor@users.noreply.github.com>
2022-12-13 11:50:11 +0200

    Update 5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""
-python script that lists all cities from the database hbtn_0e_4_usa
-with specified state name
-"""
+"""Script that takes in the name of a state as an argument
+and lists all cities"""
 
 import MySQLdb
-from sys import argv
+import sys
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cursor = db.cursor()
-    cursor.execute("SELECT cities.name FROM cities \
-    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
-    ORDER BY cities.id", (argv[4],))
-    rows = cursor.fetchall()
-    print(", ".join(city[0] for city in rows))
-    cursor.close()
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                WHERE states.name = '{}'\
+                ORDER BY cities.id ASC".format(sys.argv[4]))
+    rows = cur.fetchall()
+    print(", ".join([row[0] for row in rows]))
+    cur.close()
     db.close()
